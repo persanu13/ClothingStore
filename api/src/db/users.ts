0ca494ad76d62fd24eead7e6decff44aad2ba827
Stamate Teodor <stamateteodor23@gmail.com>
2024-05-27 20:11:22 +0300

    add setup file

---
+++ api/src/db/users.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  authentication: {
+    password: { type: String, required: true, select: false },
+    salt: { type: String, select: false },
+    sessionToken: { type: String, select: false },
+  },
+});
+
+export const UserModel = mongoose.model("User", UserSchema);
+
+export const getUsers = () => {
+  UserModel.find();
+};
+
+export const getUsersByEmail = (email: string) => {
+  UserModel.findOne({ email });
+};
+
+export const getUsersById = (id: string) => {
+  UserModel.findById(id);
+};
+
+export const CreateUser = (values: Record<string, any>) => {
+  new UserModel(values).save().then((user: any) => {
+    user.toObject();
+  });
+};
+
+export const DeleteUserById = (id: string) => {
+  UserModel.findOneAndDelete({ _id: id });
+};
+
+export const UpdateUserById = (id: string, values: Record<string, any>) => {
+  UserModel.findByIdAndUpdate(id, values);
+};
